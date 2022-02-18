162b943c25f730101cf7f2cc7bc15deafcd786e2
Ryan Dalby <dalbyryan3@gmail.com>
2022-02-17 21:26:55 -0700

    Implemented a expectimax and better evaluation function

--- P2/multiAgents.py
+++ P2/multiAgents.py
@@ -320,17 +320,112 @@ class ExpectimaxAgent(MultiAgentSearchAgent):
         legal moves.
         """
         "*** YOUR CODE HERE ***"
-        util.raiseNotDefined()
+        def get_next_agent_index_and_depth(state, agent_index, current_depth):
+            if agent_index == state.getNumAgents()-1: # We just expanded final agent 
+                next_agent_index = 0
+                next_depth = current_depth + 1
+            else:
+                next_agent_index = agent_index + 1
+                next_depth = current_depth
+            return next_agent_index, next_depth
+        
+        def max_value_action(state, agent_index, depth):
+            legal_actions = state.getLegalActions(agent_index)
+            best_value = -float('inf')
+            best_action = ""
+            next_agent_index, next_depth = get_next_agent_index_and_depth(state, agent_index, depth)
+            for action in legal_actions:
+                next_game_state = state.generateSuccessor(agent_index, action)
+                value, _ = value_action(next_game_state, next_agent_index, next_depth)
+                if (value > best_value):
+                    best_value = value
+                    best_action = action
+            return best_value, best_action
+        
+        def expected_value_action(state, agent_index, depth):
+            legal_actions = state.getLegalActions(agent_index)
+            next_agent_index, next_depth = get_next_agent_index_and_depth(state, agent_index, depth)
+            expected_value = 0
+            num_successors = len(legal_actions)
+            uniform_weight = 1.0 / num_successors
+            for action in legal_actions:
+                next_game_state = state.generateSuccessor(agent_index, action)
+                value, _ = value_action(next_game_state, next_agent_index, next_depth)
+                expected_value += value*uniform_weight
+            return expected_value, ""
+
+        def value_action(state, agent_index, depth):
+            if depth == self.depth: # At this point have completed current_depth number of turns (search plys) so should stop if we are at the set depth
+                return self.evaluationFunction(state), ""
+            legal_actions = state.getLegalActions(agent_index)
+
+            if len(legal_actions) == 0: # Terminal state
+                return self.evaluationFunction(state), ""
+            
+            is_max = (agent_index == 0) # If our current level is a max node, otherwise we are at a min node
+            if is_max:
+                return max_value_action(state, agent_index, depth)
+            else:
+                return expected_value_action(state, agent_index, depth)
+        
+        _, minimax_action = value_action(gameState, 0, 0)
+        return minimax_action
 
 def betterEvaluationFunction(currentGameState):
     """
     Your extreme ghost-hunting, pellet-nabbing, food-gobbling, unstoppable
     evaluation function (question 5).
 
-    DESCRIPTION: <write something here so we know what you did>
+    DESCRIPTION: Similar to problem 1 I utilized a combination of factors with adjustable weights. These factors were average food distance, average ghost distance, the game score, and the number of food left. 
+    They key to getting a good score was conditioning on if the ghost was scared and changing the weight of the average ghost distance to be the negative as compared to the original weight value in order to pursue the ghost and get extra points. 
     """
     "*** YOUR CODE HERE ***"
-    util.raiseNotDefined()
+    current_pos = currentGameState.getPacmanPosition()
+    current_food = currentGameState.getFood()
+    current_ghost_states = currentGameState.getGhostStates()
+    current_scared_times = [ghostState.scaredTimer for ghostState in current_ghost_states]
+
+    def get_avg_food_distance_sum(food):
+        food_list = food.asList()
+        food_sum = 0
+        count = 0
+        for f in food_list:
+            food_sum += manhattanDistance(current_pos, f)
+            count += 1
+        return food_sum/count if count > 0 else 0
+
+    def get_avg_ghost_distance_sum(ghosts):
+        ghost_sum = 0
+        count = 0
+        for g in ghosts:
+            ghost_sum += manhattanDistance(current_pos, g.getPosition())
+            count += 1
+        return ghost_sum/count if count > 0 else 0
+
+    def are_all_scared(scared_times):
+        for s in scared_times:
+            if s == 0:
+                return False
+        return True
+
+    if currentGameState.isWin():
+        return 100**100
+
+    if currentGameState.isLose():
+        return -100**100
+
+    game_score_w = 1 
+    food_w = 2 
+    ghost_w = 1 
+    num_food_w = 100
+    if are_all_scared(current_scared_times):
+        game_score_w *= 1 # Want to still be careful not to lose sight of game score
+        food_w *= 0
+        ghost_w *= -100 # Pursue the ghost rather than run away
+        num_food_w *= 0
+    
+    evaluation = (game_score_w*currentGameState.getScore()) - (food_w*get_avg_food_distance_sum(current_food)) + (ghost_w*get_avg_ghost_distance_sum(current_ghost_states) - num_food_w*len(current_food.asList()))
+    return evaluation
 
 # Abbreviation
 better = betterEvaluationFunction
